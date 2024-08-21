6ece4a33fbd5db44016c94f9725cd520ba7104fa
Mahdi Bahrami <github@mahdibm.com>
2024-08-21 16:15:31 +0330

    Handle `EmptyQueryResponse` (#500)

--- Tests/PostgresNIOTests/New/Connection State Machine/ExtendedQueryStateMachineTests.swift
+++ Tests/PostgresNIOTests/New/Connection State Machine/ExtendedQueryStateMachineTests.swift
@@ -5,16 +5,16 @@ import Logging
 @testable import PostgresNIO
 
 class ExtendedQueryStateMachineTests: XCTestCase {
-    
+
     func testExtendedQueryWithoutDataRowsHappyPath() {
         var state = ConnectionStateMachine.readyForQuery()
-        
+
         let logger = Logger.psqlTest
         let promise = EmbeddedEventLoop().makePromise(of: PSQLRowStream.self)
         promise.fail(PSQLError.uncleanShutdown) // we don't care about the error at all.
         let query: PostgresQuery = "DELETE FROM table WHERE id=\(1)"
         let queryContext = ExtendedQueryContext(query: query, logger: logger, promise: promise)
-        
+
         XCTAssertEqual(state.enqueue(task: .extendedQuery(queryContext, writePromise: nil)), .sendParseDescribeBindExecuteSync(query, promise: nil))
         XCTAssertEqual(state.parseCompleteReceived(), .wait)
         XCTAssertEqual(state.parameterDescriptionReceived(.init(dataTypes: [.int8])), .wait)
@@ -23,20 +23,20 @@ class ExtendedQueryStateMachineTests: XCTestCase {
         XCTAssertEqual(state.commandCompletedReceived("DELETE 1"), .succeedQuery(promise, with: .init(value: .noRows(.tag("DELETE 1")), logger: logger)))
         XCTAssertEqual(state.readyForQueryReceived(.idle), .fireEventReadyForQuery)
     }
-    
+
     func testExtendedQueryWithDataRowsHappyPath() {
         var state = ConnectionStateMachine.readyForQuery()
-        
+
         let logger = Logger.psqlTest
         let promise = EmbeddedEventLoop().makePromise(of: PSQLRowStream.self)
         promise.fail(PSQLError.uncleanShutdown) // we don't care about the error at all.
         let query: PostgresQuery = "SELECT version()"
         let queryContext = ExtendedQueryContext(query: query, logger: logger, promise: promise)
-        
+
         XCTAssertEqual(state.enqueue(task: .extendedQuery(queryContext, writePromise: nil)), .sendParseDescribeBindExecuteSync(query, promise: nil))
         XCTAssertEqual(state.parseCompleteReceived(), .wait)
         XCTAssertEqual(state.parameterDescriptionReceived(.init(dataTypes: [.int8])), .wait)
-        
+
         // We need to ensure that even though the row description from the wire says that we
         // will receive data in `.text` format, we will actually receive it in binary format,
         // since we requested it in binary with our bind message.
@@ -47,7 +47,7 @@ class ExtendedQueryStateMachineTests: XCTestCase {
             .init(name: $0.name, tableOID: $0.tableOID, columnAttributeNumber: $0.columnAttributeNumber, dataType: $0.dataType,
                   dataTypeSize: $0.dataTypeSize, dataTypeModifier: $0.dataTypeModifier, format: .binary)
         }
-        
+
         XCTAssertEqual(state.rowDescriptionReceived(.init(columns: input)), .wait)
         XCTAssertEqual(state.bindCompleteReceived(), .succeedQuery(promise, with: .init(value: .rowDescription(expected), logger: logger)))
         let row1: DataRow = [ByteBuffer(string: "test1")]
@@ -55,7 +55,7 @@ class ExtendedQueryStateMachineTests: XCTestCase {
         XCTAssertEqual(state.channelReadComplete(), .forwardRows([row1]))
         XCTAssertEqual(state.readEventCaught(), .wait)
         XCTAssertEqual(state.requestQueryRows(), .read)
-        
+
         let row2: DataRow = [ByteBuffer(string: "test2")]
         let row3: DataRow = [ByteBuffer(string: "test3")]
         let row4: DataRow = [ByteBuffer(string: "test4")]
@@ -65,15 +65,15 @@ class ExtendedQueryStateMachineTests: XCTestCase {
         XCTAssertEqual(state.channelReadComplete(), .forwardRows([row2, row3, row4]))
         XCTAssertEqual(state.requestQueryRows(), .wait)
         XCTAssertEqual(state.readEventCaught(), .read)
-        
+
         XCTAssertEqual(state.channelReadComplete(), .wait)
         XCTAssertEqual(state.readEventCaught(), .read)
-        
+
         let row5: DataRow = [ByteBuffer(string: "test5")]
         let row6: DataRow = [ByteBuffer(string: "test6")]
         XCTAssertEqual(state.dataRowReceived(row5), .wait)
         XCTAssertEqual(state.dataRowReceived(row6), .wait)
-        
+
         XCTAssertEqual(state.commandCompletedReceived("SELECT 2"), .forwardStreamComplete([row5, row6], commandTag: "SELECT 2"))
         XCTAssertEqual(state.readyForQueryReceived(.idle), .fireEventReadyForQuery)
     }
@@ -98,17 +98,17 @@ class ExtendedQueryStateMachineTests: XCTestCase {
 
     func testReceiveTotallyUnexpectedMessageInQuery() {
         var state = ConnectionStateMachine.readyForQuery()
-        
+
         let logger = Logger.psqlTest
         let promise = EmbeddedEventLoop().makePromise(of: PSQLRowStream.self)
         promise.fail(PSQLError.uncleanShutdown) // we don't care about the error at all.
         let query: PostgresQuery = "DELETE FROM table WHERE id=\(1)"
         let queryContext = ExtendedQueryContext(query: query, logger: logger, promise: promise)
-        
+
         XCTAssertEqual(state.enqueue(task: .extendedQuery(queryContext, writePromise: nil)), .sendParseDescribeBindExecuteSync(query, promise: nil))
         XCTAssertEqual(state.parseCompleteReceived(), .wait)
         XCTAssertEqual(state.parameterDescriptionReceived(.init(dataTypes: [.int8])), .wait)
-        
+
         let psqlError = PSQLError.unexpectedBackendMessage(.authentication(.ok))
         XCTAssertEqual(state.authenticationMessageReceived(.ok),
                        .failQuery(promise, with: psqlError, cleanupContext: .init(action: .close, tasks: [], error: psqlError, closePromise: nil)))
